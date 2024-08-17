32527a9cece6ee1e1c88dad2dbba080d02963a84
Irene <irene.jurna@gmail.com>
2024-08-17 20:05:50 +0200

    Toevoegen van bestanden voor Netlify deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from '../homepage/homepage.component';
+import { DesignSystemComponent } from '../design-system/design-system.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomepageComponent},
+    {path: 'design', component: DesignSystemComponent},
+];
