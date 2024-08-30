bee6fd2a11ddfbb6c8e5275bc6013d620e7daf7c
Irene <irene.jurna@gmail.com>
2024-08-30 15:00:05 +0200

    Navigatie aangepast en routing voor Netlify

--- src/navigation/navigation.component.ts
+++ src/navigation/navigation.component.ts
@@ -1,12 +1,30 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-navigation',
+  selector: "app-navigation",
   standalone: true,
   imports: [],
-  templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css'
+  templateUrl: "./navigation.component.html",
+  styleUrl: "./navigation.component.css",
 })
 export class NavigationComponent {
+  @ViewChild("nav") nav!: ElementRef;
+  @ViewChild("navToggle") navToggle!: ElementRef;
 
+  toggleNav() {
+    const visibility = this.nav.nativeElement.getAttribute("data-visible");
+
+    if (visibility === "false") {
+      this.nav.nativeElement.setAttribute("data-visible", "true");
+      this.navToggle.nativeElement.setAttribute("aria-expanded", "true");
+    } else {
+      this.nav.nativeElement.setAttribute("data-visible", "false");
+      this.navToggle.nativeElement.setAttribute("aria-expanded", "false");
+    }
+  }
 }
