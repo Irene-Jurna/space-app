73dcf1abf0593db40240a54338de3beaf03411ca
Irene <irene.jurna@gmail.com>
2024-10-18 15:59:53 +0200

    Start technology page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomepageComponent } from "../homepage/homepage.component";
 import { DesignSystemComponent } from "../design-system/design-system.component";
 import { DestinationComponent } from "../destination/destination.component";
 import { CrewComponent } from "../crew/crew.component";
+import { TechnologyComponent } from "../technology/technology.component";
 
 export const routes: Routes = [
   { path: "", component: HomepageComponent },
   { path: "design", component: DesignSystemComponent },
   { path: "destination", component: DestinationComponent },
   { path: "crew", component: CrewComponent },
+  { path: "technology", component: TechnologyComponent },
 ];
---
+++ src/technology/technology.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavigationComponent } from '../navigation/navigation.component';
+
+@Component({
+  selector: 'app-technology',
+  standalone: true,
+  imports: [NavigationComponent],
+  templateUrl: './technology.component.html',
+  styleUrl: './technology.component.css'
+})
+export class TechnologyComponent {
+
+}
